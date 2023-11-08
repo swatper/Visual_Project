a41239efd70bebdf48f2f082a3cabc7f3645abab
swatper <67682393+swatper@users.noreply.github.com>
2023-11-08 09:04:32 +0900

    소켓 파일 추가

---
+++ VisualProject/CMySocket.cpp
@@ -0,0 +1,54 @@
+﻿// CMySocket.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Network1.h"
+#include "CMySocket.h"
+#include "Network1Dlg.h"
+
+// CMySocket
+
+CMySocket::CMySocket()
+{
+}
+
+CMySocket::~CMySocket()
+{
+}
+
+
+// CMySocket 멤버 함수
+void CMySocket::SetParent(CDialog* pWnd)
+{
+	m_pWnd = pWnd;
+}
+
+void CMySocket::OnAccept(int nErrorCode)
+{
+	if (nErrorCode == 0)
+		((CNetwork1Dlg*)m_pWnd)->OnAccept();
+}
+
+void CMySocket::OnConnect(int nErrorCode)
+{
+	if (nErrorCode == 0)
+		((CNetwork1Dlg*)m_pWnd)->OnConnect();
+}
+
+void CMySocket::OnReceive(int nErrorCode)
+{
+	if (nErrorCode == 0)
+		((CNetwork1Dlg*)m_pWnd)->OnReceive();
+}
+
+void CMySocket::OnClose(int nErrorCode)
+{
+	if (nErrorCode == 0)
+		((CNetwork1Dlg*)m_pWnd)->OnClose();
+}
+
+void CMySocket::OnSend(int nErrorCode)
+{
+	if (nErrorCode == 0)
+		((CNetwork1Dlg*)m_pWnd)->OnSend();
+}
